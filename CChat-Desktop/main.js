76798d4f6410c17050635f49d3ae01410778c71a
Xavi <xavi@Xavis-MacBook-Pro.local>
2026-09-11 14:14:35 +1000

    Integrate desktop client application

---
+++ CChat-Desktop/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    titleBarStyle: 'hiddenInset',
+    backgroundColor: '#0B0F17',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
